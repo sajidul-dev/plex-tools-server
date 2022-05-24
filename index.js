4535a8ff4351504a89a875640bc9d085807de872
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-25 02:57:15 +0600

    Put order in order collection implemented

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function run() {
     try {
         await client.connect();
         const toolCollection = client.db('plex_tools').collection('tools')
+        const orderCollection = client.db('plex_tools').collection('orders')
 
         // Get Six items
         app.get('/tools', async (req, res) => {
@@ -31,10 +32,29 @@ async function run() {
         })
 
         // find one item for puchase page
-        app.get('/tools/:id', async (req, res) => {
+        app.get('/tool/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await toolCollection.findOne(query)
+            res.send(result)
+        })
+
+        // put order in orders collection
+        app.put('/order', async (req, res) => {
+            const order = req.body
+            const filter = { toolId: order.toolId }
+            const exist = await orderCollection.findOne(filter)
+            const options = { upsert: true };
+            if (exist) {
+                const updatedDoc = {
+                    $set: {
+                        quantity: exist.quantity + order.quantity
+                    }
+                }
+                const updatedOrder = await orderCollection.updateOne(filter, updatedDoc, options)
+                res.send(updatedOrder)
+            }
+            const result = await orderCollection.insertOne(order)
             res.send(result)
         })
     }
