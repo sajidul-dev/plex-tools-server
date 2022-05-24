c3c80d7f6b496893064b2271785579f9091638ba
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-25 03:14:37 +0600

    JWT implemented

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
@@ -12,12 +13,45 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l9moh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        res.status(401).send({ message: "Unauthorized access" })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    });
+}
+
 async function run() {
     try {
         await client.connect();
         const toolCollection = client.db('plex_tools').collection('tools')
         const orderCollection = client.db('plex_tools').collection('orders')
+        const userCollection = client.db('plex_tools').collection('users')
 
+
+
+
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ result, token })
+        })
         // Get Six items
         app.get('/tools', async (req, res) => {
             const query = {}
